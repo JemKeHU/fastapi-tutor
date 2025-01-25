61dcfc8547fb8233868ec2b968796d59bef216ce
JemKeHU <staslovel1338@gmail.com>
2025-01-25 21:57:30 +0000

    Tried to create a multiple pages

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ my_tutorial_app = FastAPI()
 @my_tutorial_app.get("/")
 def root():
     return FileResponse("public/index.html")
+
+@my_tutorial_app.get("/file", response_class=FileResponse)
+def root_app():
+    return FileResponse("public/another_index.html")
