364edd3eeadf696e563067b1d2280625dc8a8b03
JemKeHU <staslovel1338@gmail.com>
2025-01-25 21:47:43 +0000

    Created a get response to a page

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 my_tutorial_app = FastAPI()
 
 @my_tutorial_app.get("/")
-async def root():
-    return {"message": "Hello from FastAPI!"}
+def root():
+    return FileResponse("public/index.html")
